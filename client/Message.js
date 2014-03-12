e8e5b1a12f981da1d0b2f81f47375e302bb55437
Caleb James DeLisle <cjd@hyperboria.ca>
2014-03-12 09:43:03 +0100

    Add DISCONNECT message so when other users disconnect it can be transmitted back.

--- client/Message.js
+++ client/Message.js
@@ -23,6 +23,7 @@ var Message = module.exports;
 var REGISTER     = Message.REGISTER     = 0;
 var REGISTER_ACK = Message.REGISTER_ACK = 1;
 var PATCH        = Message.PATCH        = 2;
+var DISCONNECT   = Message.DISCONNECT   = 3;
 
 var check = Message.check = function(msg) {
     Common.assert(msg.type === 'Message');
@@ -33,7 +34,10 @@ var check = Message.check = function(msg) {
     if (msg.messageType === PATCH) {
         Patch.check(msg.content);
         Common.assert(typeof(msg.lastMsgHash) === 'string');
-    } else if (msg.messageType === REGISTER || msg.messageType === REGISTER_ACK) {
+    } else if (msg.messageType === REGISTER
+        || msg.messageType === REGISTER_ACK
+        || msg.messageType === DISCONNECT)
+    {
         Common.assert(typeof(msg.lastMsgHash) === 'undefined');
         Common.assert(typeof(msg.content) === 'undefined');
     } else {
@@ -111,4 +115,3 @@ var hashOf = Message.hashOf = function (msg) {
     msg.authToken = authToken;
     return hash;
 };
-
